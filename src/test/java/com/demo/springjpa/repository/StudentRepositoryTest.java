43dccc91c6d89796409c8da1d3bbf64848d0a729
Your Name <moseoduor86@gmail.com>
2021-08-23 10:34:35 +0300

    first commit

---
+++ src/test/java/com/demo/springjpa/repository/StudentRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.demo.springjpa.repository;
+
+import com.demo.springjpa.entity.Student;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StudentRepositoryTest {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Test
+    public void saveStudent(){
+        Student student = Student.builder()
+                .emailId("mosesa@gmail.co.ke")
+                .firstName("moses")
+                .lastName("oketch")
+                .guardianName("james")
+                .guardianEmail("james@gmail.co.ke")
+                .guardianMobile("073387987")
+                .build();
+        studentRepository.save(student);
+    }
+
+}
