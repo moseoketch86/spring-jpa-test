8a6c860da80e7bd9ef55a82f01f629acf65d8ce0
Your Name <moseoduor86@gmail.com>
2021-08-23 10:55:25 +0300

    test for printing student in the database

--- src/test/java/com/demo/springjpa/repository/StudentRepositoryTest.java
+++ src/test/java/com/demo/springjpa/repository/StudentRepositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -24,6 +26,11 @@ class StudentRepositoryTest {
                 .guardianMobile("073387987")
                 .build();
         studentRepository.save(student);
+    }
+    @Test
+    public void printAllStudent(){
+        List<Student> studentList = studentRepository.findAll();
+        System.out.println(studentList);
     }
 
 }
