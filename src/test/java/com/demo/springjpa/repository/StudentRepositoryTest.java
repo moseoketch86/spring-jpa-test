1affe5fe8531b2343196e46b85d717cf41d95cea
Your Name <moseoduor86@gmail.com>
2021-08-23 11:51:30 +0300

    test for saving student with guardian as an object

--- src/test/java/com/demo/springjpa/repository/StudentRepositoryTest.java
+++ src/test/java/com/demo/springjpa/repository/StudentRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.demo.springjpa.repository;
 
+import com.demo.springjpa.entity.Guardian;
 import com.demo.springjpa.entity.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,9 +22,9 @@ class StudentRepositoryTest {
                 .emailId("mosesa@gmail.co.ke")
                 .firstName("moses")
                 .lastName("oketch")
-                .guardianName("james")
-                .guardianEmail("james@gmail.co.ke")
-                .guardianMobile("073387987")
+               // .guardianName("james")
+               // .guardianEmail("james@gmail.co.ke")
+               // .guardianMobile("073387987")
                 .build();
         studentRepository.save(student);
     }
@@ -31,6 +32,23 @@ class StudentRepositoryTest {
     public void printAllStudent(){
         List<Student> studentList = studentRepository.findAll();
         System.out.println(studentList);
+    }
+    @Test
+    public void saveStudentWithGuardian(){
+        Guardian guardian = Guardian.builder()
+                .email("guardian@gmail.co.ke")
+                .name("john")
+                .mobile("0568878")
+                .build();
+
+        Student student = Student.builder()
+                .emailId("ken@gmail.co.ke")
+                .firstName("ken")
+                .lastName("oketch")
+                .guardian(guardian)
+                .build();
+        studentRepository.save(student);
+
     }
 
 }
